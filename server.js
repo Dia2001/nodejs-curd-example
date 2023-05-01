abdb3279be5fae475ba416995c8bc04493a5ad69
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2023-05-01 20:44:27 +0700

    feat: creat data succes

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3005"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json())
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./src/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+
+require("./src/routes/tutorial.routes.js")(app);
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Demo curd with nodejs" });
+  });
+  
+
+// set port, listen for requests
+const PORT = 3005;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ src/routes/tutorial.routes.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+    const tutorials = require("../controller/tutorial.controller");
+  
+    var router = require("express").Router();
+  
+    // Create a new Tutorial
+    router.post("/", tutorials.create);
+    app.use("/api/tutorials", router);
+};
